bb71fb9a71cb26bb606ca8368d5a4412040f056b
Mikhail Kharitonov <95705443+MAKharitonov@users.noreply.github.com>
2023-08-09 13:08:26 +0300

    ref_mat_model complicated

--- src/converters/complicated.py
+++ src/converters/complicated.py
@@ -27,16 +27,19 @@ class Complicated:
     def __p(self):
         return int(((self.__n + 1) * (self.__n + 2 * self.__m) + 2) / 2)
 
-    def __calc_n_i_j(self, i, j):
+    def __calc_location_flow_type_n_from_node_i_to_j(self, i, j):
         return int((self.__n - 1) * i - ((i - 1) * i) / 2 + j - 1)
 
-    def __calc_r_l_i(self, l, i):
-        return int(self.__calc_n_i_j(self.__n - 1, self.__n) + (l - 1) * self.__n + i)
+    def __calc_location_flow_type_r_from_node_l_to_j(self, l, i):
+        return int(
+            self.__calc_location_flow_type_n_from_node_i_to_j(self.__n - 1,
+                                                              self.__n) +
+                   (l - 1) * self.__n + i)
 
-    def __calc_n_f(self, i):
+    def __calc_location_flow_type_n_from_node_i_to_f(self, i):
         return int(self.__p - 1 - (self.__m - 1) - (self.__n + 1) + i)
 
-    def __calc_r_f(self, i):
+    def __calc_location_flow_type_r_from_node_i_to_f(self, i):
         return int(self.__p - 1 - (self.__m - 1) + i - 1)
 
     def __build_objective_function_coefficients(self):
@@ -49,48 +52,55 @@ class Complicated:
         return self.__fm
 
     def __build_constraint_matrix(self):
-        for i in np.arange(1, self.__n + 1):
-            self.__cm[0][self.__calc_n_i_j(0, i)] = 1.0
-            self.__cm[0][self.__calc_n_f(0)] = 1.0
-            for l in np.arange(1, self.__m):
-                self.__cm[l][self.__calc_r_l_i(l, i)] = self.__cm[l][self.__calc_r_f(l)] = 1.0
+        n = self.__n
+        m = self.__m
+        a = self.__a
+        c = self.__cm
+        u = self.__u
+        p = self.__p
+        n_ij = self.__calc_location_flow_type_n_from_node_i_to_j
+        n_f = self.__calc_location_flow_type_n_from_node_i_to_f
+        r_li = self.__calc_location_flow_type_r_from_node_l_to_j
+        r_f = self.__calc_location_flow_type_r_from_node_i_to_f
 
-        for i in np.arange(1, self.__n):
+        for i in np.arange(1, n + 1):
+            c[0][n_ij(0, i)] = 1.0
+            c[0][n_f(0)] = 1.0
+            for l in np.arange(1, m):
+                c[l][r_li(l, i)] = 1.0
+                c[l][r_f(l)] = 1.0
+
+        for i in np.arange(1, n):
             for l in np.arange(0, i):
-                self.__cm[self.__m + self.__m * (i - 1)][self.__calc_n_i_j(l, i)] = \
-                    - float(self.__a.sum(axis=1)[i] / self.__a[i][0])
+                c[m + m * (i - 1)][n_ij(l, i)] = - float(a.sum(axis=1)[i] / a[i][0])
 
-            for j in np.arange(i + 1, self.__n + 1):
-                self.__cm[self.__m + self.__m * (i - 1)][self.__calc_n_i_j(i, j)] = 1.0
-            self.__cm[self.__m + self.__m * (i - 1)][self.__calc_n_f(i)] = 1.0  # 1 заменить на e_i
+            for j in np.arange(i + 1, n + 1):
+                c[m + m * (i - 1)][n_ij(i, j)] = 1.0
 
-            for k in np.arange(1, self.__m):
-                self.__cm[self.__m + self.__m * (i - 1) + k][self.__calc_r_l_i(k, i)] = \
-                    - float(self.__a.sum(axis=1)[i] / self.__a[i][k])
+            c[m + m * (i - 1)][n_f(i)] = 1.0  # 1 заменить на e_i
 
-                for j in np.arange(i + 1, self.__n + 1):
-                    self.__cm[self.__m + self.__m * (i - 1) + k][self.__calc_n_i_j(i, j)] = 1.0
-                self.__cm[self.__m + self.__m * (i - 1) + k][self.__calc_n_f(i)] = 1.0
+            for k in np.arange(1, m):
+                c[m + m * (i - 1) + k][r_li(k, i)] = - float(a.sum(axis=1)[i] / a[i][k])
 
-        for l in np.arange(0, self.__n):
-            self.__cm[self.__u - 2 * self.__m][self.__calc_n_i_j(l, self.__n)] = \
-                - float(self.__a.sum(axis=1)[self.__n] / self.__a[self.__n][0])
+                for j in np.arange(i + 1, n + 1):
+                    c[m + m * (i - 1) + k][n_ij(i, j)] = 1.0
+                c[m + m * (i - 1) + k][n_f(i)] = 1.0
 
-        self.__cm[self.__u - 2 * self.__m][self.__calc_n_f(self.__n)] = 1.0
-        for k in np.arange(1, self.__m):
-            self.__cm[self.__u - 2 * self.__m + k][self.__calc_r_l_i(k, self.__n)] = \
-                - float(self.__a.sum(axis=1)[self.__n] / self.__a[self.__n][k])
-            self.__cm[self.__u - 2 * self.__m + k][self.__calc_n_f(self.__n)] = 1.0
+        for l in np.arange(0, n):
+            c[u - 2 * m][n_ij(l, n)] = - float(a.sum(axis=1)[n] / a[n][0])
 
-        for i in np.arange(0, self.__n + 1):
-            self.__cm[self.__u - self.__m][self.__calc_n_f(i)] = \
-                - float(self.__a.sum(axis=1)[0] / self.__a[0][0])
-            self.__cm[self.__u - self.__m][self.__p - 1] = 1.0
+        c[u - 2 * m][n_f(n)] = 1.0
+        for k in np.arange(1, m):
+            c[u - 2 * m + k][r_li(k, n)] = - float(a.sum(axis=1)[n] / a[n][k])
+            c[u - 2 * m + k][n_f(n)] = 1.0
 
-        for k in np.arange(1, self.__m):
-            self.__cm[self.__u - self.__m + k][self.__calc_r_f(k)] = \
-                - float(self.__a.sum(axis=1)[0] / self.__a[0][k])
-            self.__cm[self.__u - self.__m + k][self.__p - 1] = 1.0
+        for i in np.arange(0, n + 1):
+            c[u - m][n_f(i)] = - float(a.sum(axis=1)[0] / a[0][0])
+            c[u - m][p - 1] = 1.0
 
-        return self.__cm
+        for k in np.arange(1, m):
+            c[u - m + k][r_f(k)] = - float(a.sum(axis=1)[0] / a[0][k])
+            c[u - m + k][p - 1] = 1.0
+
+        return c
 
